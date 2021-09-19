024adea596fc8d3a8aafa7135a8c094087396d8f
KZTahat <kztahat96@gmail.com>
2021-09-19 14:45:18 +0300

    Done

---
+++ 401-course/week5/resty-app/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./app.scss";
+import Header from "./componenets/Header";
+import Form from "./componenets/form/Form";
+import Results from "./componenets/Results";
+import Footer from "./componenets/Footer";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      requestParams: {},
+    };
+  }
+
+  callApi = (requestParams) => {
+    const data = {
+      count: 2,
+      results: [
+        { name: "fake thing 1", url: "http://fakethings.com/1" },
+        { name: "fake thing 2", url: "http://fakethings.com/2" },
+      ],
+    };
+    this.setState({ data, requestParams });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <div>Request Method: {this.state.requestParams.method}</div>
+        <div>URL: {this.state.requestParams.url}</div>
+        <Form handleApiCall={this.callApi} />
+        <Results data={this.state.data} />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
